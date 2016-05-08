d5d454c56aecb9fc245a3954b71bce73d462bbc6
nanjiye123 <1902193503@qq.com>
2016-05-08 14:16:20 +0800

    add 007.py

--- 001回归/007SVM_kernel.py
+++ 001回归/007SVM_kernel.py
@@ -3,22 +3,25 @@ from numpy import *
 import pylab as pl
 #本程序是SVM的高斯内核版本
 
-def kernelTrans(X, A, kTup): #calc the kernel or transform data to a higher dimensional space
+def kernelTrans(X, A, kTup): 
+	#核函数将数据转换到高维空间，将2维转换到m维
 	#kTup是一个元组，第一个参数给出核函数类型的一个字符串
+	#X是所有数据点，A是一个数据点1×2
 	m,n = shape(X)
-	K = mat(zeros((m,1)))
+	K = mat( zeros((m,1)) ) #K m行1列 列向量
 	#=================================================
 	if kTup[0] == 'lin': 
 		K = X * A.T   #线性核函数
 	elif kTup[0] == 'rbf':
-		for j in range(m):
-			deltaRow = X[j,:] - A
-			K[j] = deltaRow * deltaRow.T
-		K = exp( K / (-1 * kTup[1]**2) ) #divide in NumPy is element-wise not matrix like Matlab
+		for j in range(m):                #遍历所有数据点
+			deltaRow = X[j,:] - A         #xj-xi 1*2
+			K[j] = deltaRow * deltaRow.T  #(1*2)(2*1)=(1*1)是一个数
+		K = exp( K / (-1 * kTup[1]**2) )  #K是一个列向量，除以一个数
+		#numpy的除法是数相除
 	else: 
-		raise NameError('Houston We Have a Problem, That Kernel is not recognized')
+		raise NameError('核函数类型输入有误')
 	#=================================================
-	return K
+	return K #K m*1 列向量
 
 class optStruct: #高斯核版本
 	def __init__(self, dataMatIn, classLabels, C, toler, kTup):
@@ -34,6 +37,8 @@ class optStruct: #高斯核版本
 		self.K = mat( zeros((self.m, self.m)) ) #K是m*m的矩阵
 		for i in range(self.m):	
 			self.K[:,i] = kernelTrans(self.X, self.X[i,:], kTup)
+			#核函数将2维转换到m维
+			#K的每一列是转换后的一个数据点
 
 def clipAlpha(aj,H,L):#用于调整alpha值，使其范围是H到L
     if aj > H: 
@@ -43,7 +48,7 @@ def clipAlpha(aj,H,L):#用于调整alpha值，使其范围是H到L
     return aj
 
 def calcEk(oS, k): #误差值计算
-	fxk = float( multiply(oS.alphas, oS.labelMat).T * (oS.X * oS.X[k,:].T)) + oS.b
+	fxk = float( multiply(oS.alphas, oS.labelMat).T * oS.K[:,k] + oS.b)
 	#alphas 100*1, labelMat 100*1, X 100*2 
 	#print shape(fxk)
 	Ek = fxk - float(oS.labelMat[k])
@@ -82,13 +87,13 @@ def innerL(i, oS):
 	#如果误差很大，需要对alpha进行优化，判断误差大小的条件如下
 	if ((oS.labelMat[i]*Ei < -oS.tol) and (oS.alphas[i] < oS.C)) or ((oS.labelMat[i]*Ei > oS.tol) and (oS.alphas[i] > 0)):
 	#判断正间隔或负间隔<tol容错率 且 判断alpha是否在0到C内，而不是等于0或C。因为等于0或C就是在边界，不用优化。
-		j,Ej = selectJ(i, oS, Ei) #第二个alpha选择中的启发式算法，不是随机选择
+		j,Ej = selectJ(i, oS, Ei)         #第二个alpha选择中的启发式算法，不是随机选择
 		#==================================================
 		#python通过引用传递列表，所以必须为alphaIold和alphaJold分配内存
 		alphaIold = oS.alphas[i].copy()   #复制第一个alpha的值，作为老的alphai
 		alphaJold = oS.alphas[j].copy()   #复制第二个alpha的值，作为老的alphaj
 		#===================================================
-		if (oS.labelMat[i] != oS.labelMat[j]): #判断label同号还是异号
+		if (oS.labelMat[i] != oS.labelMat[j]):       #判断label同号还是异号
 			L = max(0, oS.alphas[j] - oS.alphas[i])  #计算alpha2的上下限
 			H = min(oS.C, oS.C + oS.alphas[j] - oS.alphas[i])
 		else: #label异号
@@ -100,7 +105,7 @@ def innerL(i, oS):
 			return 0
 		#===================================================
 		#eta是alphaj的最优修改量，大于等于0时不更新，退出
-		eta = 2.0 * oS.X[i,:] * oS.X[j,:].T - oS.X[i,:] * oS.X[i,:].T - oS.X[j,:] * oS.X[j,:].T
+		eta = 2.0 * oS.K[i,j] - oS.K[i,i] - oS.K[j,j]
 		if eta >= 0: 
 			print "eta>=0"
 			return 0
@@ -116,10 +121,10 @@ def innerL(i, oS):
 		updateEk(oS, i) #更新alphai的误差值Ecache
 		#===================================================
 		#求b
-		b1 = oS.b - Ei - oS.labelMat[i] * (oS.alphas[i] - alphaIold) * oS.X[i,:] * oS.X[i,:].T - \
-			 oS.labelMat[j] * (oS.alphas[j] - alphaJold) * oS.X[i,:] * oS.X[j,:].T
-		b2 = oS.b - Ej - oS.labelMat[i] * (oS.alphas[i] - alphaIold) * oS.X[i,:] * oS.X[j,:].T - \
-			 oS.labelMat[j] * (oS.alphas[j] - alphaJold) * oS.X[j,:] * oS.X[j,:].T
+		b1 = oS.b - Ei - oS.labelMat[i] * (oS.alphas[i] - alphaIold) * oS.K[i,i] - \
+			 oS.labelMat[j] * (oS.alphas[j] - alphaJold) * oS.K[i,j]
+		b2 = oS.b - Ej - oS.labelMat[i] * (oS.alphas[i] - alphaIold) * oS.K[i,j] - \
+			 oS.labelMat[j] * (oS.alphas[j] - alphaJold) * oS.K[j,j]
 		if (0 < oS.alphas[i]) and (oS.C > oS.alphas[i]): 
 			oS.b = b1
 		elif (0 < oS.alphas[j]) and (oS.C > oS.alphas[j]): 
@@ -134,8 +139,8 @@ def innerL(i, oS):
 
 #完整版Platt SMO外循环代码
 #函数输入：数据集，类别标签，惩罚因子，容错率，最大循环次数
-def smoP(dataMatIn, classLabels, C, toler, maxIter):
-	oS = optStruct(mat(dataMatIn), mat(classLabels).T, C, toler)#构建数据结构，存储所有数据
+def smoP(dataMatIn, classLabels, C, toler, maxIter, kTup):
+	oS = optStruct(mat(dataMatIn), mat(classLabels).T, C, toler, kTup)#构建数据结构，存储所有数据
 	iter = 0                                      #迭代次数
 	entireSet = True                              #标志整个数据集
 	alphaPairsChanged = 0                         #记录优化的alpha个数
@@ -197,51 +202,56 @@ def drawpict(dataArr, labelArr, w = 0, b = 0):
 			pl.plot(x[i,0], x[i,1], 'ro')
 		else:
 			pl.plot(x[i,0], x[i,1], 'bs')
-
-	xx = [2,3,4,5,6]
-	yy = []
-	for i in range(shape(xx)[0]):
-		yy.append( float((-w[0,0] * xx[i] - b) / w[1,0]) )
-	pl.plot(xx, yy)
 	pl.xlabel('x1')
 	pl.ylabel('x2')
 	pl.show()
 
 def testRbf(k1 = 1.3): #参数k1是核函数中的到达率/函数值跌落速度
+	#k1对结果的影响：
+	#k1 = 0.1 时，核函数减少了每个支持向量的影响程度，因此需要更多的支持向量，支持向量的数目增加
+	#k1 = 1.3 时，支持向量数目减少，且这些支持向量在决策边界的周围，此时测试错误率也下降
+	#降低k1，训练错误率会降低，测试错误率会上升
+	#===================================================================
+	#   使用训练数据集
 	dataArr,labelArr = loadData('data007.txt') #返回的是列表
 	b,alphas = smoP(dataArr, labelArr, 200, 0.0001, 10000, ('rbf', k1))
 	#惩罚因子C=200(重要)，容错率toler=0.0001，最大循环次数10000，核函数类型rbf
-	datMat=mat(dataArr) #datMat 100*2
-	labelMat = mat(labelArr).transpose() #labelMat 100*1
-	svInd=nonzero(alphas.A>0)[0] #返回大于0的alpha的行号，即支持向量
-	sVs=datMat[svInd] #得到支持向量的数据点
-	labelSV = labelMat[svInd]; #得到支持向量的标签
+	datMat=mat(dataArr)                   #datMat 100*2
+	labelMat = mat(labelArr).transpose()  #labelMat 100*1
+	svInd=nonzero(alphas.A>0)[0]          #返回大于0的alpha的行号，即支持向量
+	sVs=datMat[svInd]                     #得到支持向量的数据点
+	labelSV = labelMat[svInd];            #得到支持向量的标签
 	print "there are %d Support Vectors" % shape(sVs)[0]
 	m,n = shape(datMat)
 	errorCount = 0
-	for i in range(m):
-		kernelEval = kernelTrans(sVs,datMat[i,:],('rbf', k1))
-		predict=kernelEval.T * multiply(labelSV,alphas[svInd]) + b
+	#-------------------------------------------------------------
+	for i in range(m):#利用核函数进行分类
+	#只需要支持向量数据就可以进行分类
+		kernelEval = kernelTrans(sVs,datMat[i,:],('rbf', k1))      #求出类似于w的向量
+		predict=kernelEval.T * multiply(labelSV,alphas[svInd]) + b #求出预测标签值
 		if sign(predict)!=sign(labelArr[i]): errorCount += 1
 	print "the training error rate is: %f" % (float(errorCount)/m)
+	#-------------------------------------------------------------
 	#====================================================================
+	#    使用测试数据集
 	dataArr,labelArr = loadData('data007_2.txt')
 	errorCount = 0
 	datMat=mat(dataArr); labelMat = mat(labelArr).transpose()
 	m,n = shape(datMat)
-	for i in range(m):
+	#-------------------------------------------------------------
+	for i in range(m):#利用核函数进行分类
+	#只需要支持向量数据就可以进行分类
 		kernelEval = kernelTrans(sVs,datMat[i,:],('rbf', k1))
 		predict=kernelEval.T * multiply(labelSV,alphas[svInd]) + b
 		if sign(predict)!=sign(labelArr[i]): errorCount += 1    
 	print "the test error rate is: %f" % (float(errorCount)/m)  
-
+	#-------------------------------------------------------------
+	#====================================================================
 
 if  __name__ == "__main__":
-	testRbf()
-	#dataArr, labelArr = loadData("data006.txt")#返回的是列表
-	#b, alphas = smoP(dataArr, labelArr, 0.6, 0.001, 40)
-	#惩罚因子C=0.6   容错率toler=0.001   最大循环次数maxIter=40
-	#w = calW(alphas, dataArr, labelArr)
-	#drawpict(dataArr, labelArr, w, b)
-
+	testRbf(1.3)
+	dataArr, labelArr = loadData("data007.txt")#返回的是列表
+	drawpict(dataArr, labelArr)
+	dataArr, labelArr = loadData("data007_2.txt")
+	drawpict(dataArr, labelArr)
 
